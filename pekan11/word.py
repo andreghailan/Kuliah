95ad5203ff67bdc1f4d3ceec9dc752536ed72af6
andreghailan <andreghailan3@gmail.com>
2025-12-05 08:26:06 +0700

    streamlit

---
+++ pekan11/word.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.header("Hello, Streamlit!")
+
+with st.form("my_form"):
+    nama = st.text_input("Nama :")
+    alamat = st.text_input("Alamat : ")
+    usia = st.number_input("Usia :", min_value=0)
+
+    submit = st.form_submit_button("Submit")
+
+if submit:
+    st.success(f"Nama kamu {nama}, alamat kamu di {alamat}, dan usia kamu {usia} tahun.")
