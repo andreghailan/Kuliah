4f94257d1fc89178e9733ba991e282462cfea47f
andreghailan <andreghailan3@gmail.com>
2025-12-05 08:15:20 +0700

    pekan-11

---
+++ Pekan-10/asdos1.py
@@ -0,0 +1,8 @@
+import math
+
+
+
+print(math.sqrt(144))
+print(math.pi)
+print(math.sin(30))
+print(math.radians(30))
---
+++ Pekan-10/asdos4.py
@@ -0,0 +1,4 @@
+import datetime as dt
+
+dt_now = dt.datetime.now()
+print("Current date and time:", dt_now)
